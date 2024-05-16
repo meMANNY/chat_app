0367b87452a3cffe5e46e80fd8ecfedb0de11024
Aman Tripathi <amanttripathi02@gmail.com>
2024-05-16 18:06:45 +0530

    "created a static web page"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-
+app.use(express.static('/public'));
 const server= http.createServer(app);
 
 server.listen(3000, () => {console.log("server is running on port 3000")});
